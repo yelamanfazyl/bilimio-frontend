cd8cd58053e84b8f9020334ee9ce97b615e4a675
Yelaman Fazyl <44122015+yelamanfazyl@users.noreply.github.com>
2023-07-26 01:36:07 +0600

    tsconfig updated

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Hero/Hero";
 import Layout from "@/components/Layout/Layout";
 import React from "react";
 
-const Main: React.FC = () => {
+const Main = () => {
   return (
     <Layout>
       <Hero />
